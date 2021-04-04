d53c145302632e1c69f3c8719c3fedbba78f3c4d
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-04-04 16:22:36 -0500

    define svg are and margins

--- d3_data_journalism/assets/js/app.js
+++ d3_data_journalism/assets/js/app.js
@@ -1,1 +1,13 @@
 // @TODO: YOUR CODE HERE!
+
+//Define SVG area dimensions
+var svgWidth= 960;
+var svgHeight= 500;
+
+//Define the chart's margins as an object
+var margin= {
+	top: 20,
+	right: 20,
+	bottom, 20,
+	left: 20
+};
